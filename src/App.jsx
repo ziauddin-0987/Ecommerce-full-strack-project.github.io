649f362ee7c79c60c992dd7b4309456912e49945
ziauddin-0987 <ziauddink100@gmail.com>
2024-12-10 18:56:58 +0500

    i add routering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-95  l text-red-600">hallo zia shahab</h1>
